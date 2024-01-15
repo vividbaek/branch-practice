7bbac342e03a708274c64d8e6704f0869eccd877
백형준 <138356797+vividbaek@users.noreply.github.com>
2024-01-15 12:05:36 +0900

    feat: Change in in range() 6 to 7

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-<<<<<<< HEAD
 from random import randint
 from random import choice
 
 def get_luckies() -> list:
-	return [randint(1,45) for _ in range(6)]
-=======
+	return [randint(1,45) for _ in range(7)]
 
 
 def get_luckies() -> list:
-	return [choice(range(1,45+1)) for _ in range(6)]
+	return [choice(range(1,45+1)) for _ in range(7)]
 
 
 if __name__=='__main__':
