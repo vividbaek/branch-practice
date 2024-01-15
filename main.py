6c191bbe8f4c4d6b973a80d9edbbb12fe03c877d
백형준 <138356797+vividbaek@users.noreply.github.com>
2024-01-15 11:49:34 +0900

    feat Adjust Integer Range 1,45 to 1,50

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-<<<<<<< HEAD
 from random import randint
 from random import choice
 
-def get_luckies() -> list:
-	return [randint(1,45) for _ in range(6)]
-=======
 
+def get_randint() -> list:
+	return [randint(1,50) for _ in range(6)]
 
-def get_luckies() -> list:
-	return [choice(range(1,45+1)) for _ in range(6)]
+def get_choice() -> list:
+	return [choice(range(1,50+1)) for _ in range(6)]
 
 
 if __name__=='__main__':
-	print(get_luckies())
-    print(get_choice())
+	print(get_randint())
+	print(get_choice())
